67a4f5e938100ec26fbed2184bd4335d12754f1a
abuzarraziqgithub <abuzarraziqo@gmail.com>
2024-07-11 17:49:38 +0500

    stepsOfBuildProject

---
+++ Lectures/howToPlanProject.js
@@ -0,0 +1,22 @@
+//? How to Plan a Web Project?
+
+/*
+ * There are many ways of planning a Project and there are different organizations who use these techniques to make a powerful project.
+* The most useful way of planning a web project is divided into 4 steps:
+   ? 1- User Stories
+                    * It is the first step of a project plan is the User Stories, It is the Description of the application's functionality from the user's perspective.
+                    * All user stories put together describe the entire application.
+  ? 2- Features
+                * After describing application functionality by the user perspective, we developers can implement that to add features to our application.
+                * It is a great way of developing new features for applications.
+  ? 3- FlowChart
+                * After User stories and Features ideas, we can make a flow chart, which is the consise and clear way of developing application's functionality fastly and easily.
+                * Flowchart give us a real picture and flow of our application.
+                * This the chart of What we will build.
+  ? 4- Architecture 
+                * After making a flowchart of features with the help of user stories, the final step is to make an archtecture for our application.
+                * The architecture simply describe the design, organization of code files and coding paradigms, which we can implement to make a complex project with ease.
+                * This is How we will build it.
+  ? 5- Development Step
+                * After getting a user stories to identify features, making a flow chart and architecture, we are ready to start making our Project. 
+*/
