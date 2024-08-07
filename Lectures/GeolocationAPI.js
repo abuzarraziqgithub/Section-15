2ab9903b0422b0cdc9e5269e40edccc66472a3b2
abuzarraziqgithub <abuzarraziqo@gmail.com>
2024-08-07 17:51:39 +0500

    improved

--- Lectures/GeolocationAPI.js
+++ Lectures/GeolocationAPI.js
@@ -1,10 +1,10 @@
 "use strict";
 
-//* The geolocation API is a browser API by which we can navigate the location of a place.
+//* The geolocation API is a browser API by which we can navigate the position of a location.
 
 //* we pass two call back functions to the getCurrentPosition method of the geolocation object.
 //* The first call back function is the success call back, and the second is the error call back function.
-//* As the name itself means when the data and location is accurate(whenever the browser gets the current position of the user) then the success call back function will be called otherwise the error message by the call beack will be executed.
+//* As the name itself means when the data and location is accurate(whenever the browser gets the current position of the user) then the success call back function will be executed otherwise the error message by the call back will be executed.
 
 //* The first callback function will take a position parameter.
 //* In the second call back function, we can specify a message of error.
@@ -14,8 +14,7 @@ navigator.geolocation.getCurrentPosition(
     //* We can get the longitude and latitude using destructuring, the coords is the child object of the GeolocationPosition.
     //* This could be not an accurate coordinates.
     //* We can check our longitude and latitude with google map url, in which we will specify our coordinates.
-    const { latitude } = position.coords;
-    const { longitude } = position.coords;
+    const { latitude, longitude } = position.coords;
     console.log("Latitude:", latitude);
     console.log("Longitude:", longitude);
 
